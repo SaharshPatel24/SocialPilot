26c237fdea63100e2e9c102c9334d02237561c39
SaharshPatel24 <srpatel56@myseneca.ca>
2023-04-22 07:31:09 -0400

    fix: refactored login page code

---
+++ components/LoginPage/loginPage.tsx
@@ -0,0 +1,230 @@
+import React from 'react'
+import { Box, useToast, chakra, Icon, useColorModeValue, Image } from '@chakra-ui/react'
+import { useAuth } from '../../context/AuthProvider'
+
+const LoginPage = () => {
+    const toast = useToast();
+    const bg = useColorModeValue("white", "gray.800");
+    const { login, error } = useAuth();
+
+    const handleLogin = async () => {
+        if(!error){
+          await login();
+        } else {
+          toast({
+            title: error,
+            status: 'error',
+            duration: 6000,
+            isClosable: true,
+          })
+        }
+      };
+
+    return (
+        <Box pos="relative" overflow="hidden" bg={bg} mt={10}>
+      <Box maxW="7xl" mx="auto">
+        <Box
+          pos="relative"
+          pb={{
+            base: 8,
+            sm: 16,
+            md: 20,
+            lg: 28,
+            xl: 32,
+          }}
+          maxW={{
+            lg: "2xl",
+          }}
+          w={{
+            lg: "full",
+          }}
+          zIndex={1}
+          bg={bg}
+          border="solid 1px transparent"
+        >
+          <Icon
+            display={{
+              base: "none",
+              lg: "block",
+            }}
+            position="absolute"
+            right={0}
+            top={0}
+            bottom={0}
+            h="full"
+            w={48}
+            color={bg}
+            transform="translateX(50%)"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </Icon>
+          <Box
+            mx="auto"
+            maxW={{
+              base: "8xl",
+            }}
+            px={{
+              base: 4,
+              sm: 6,
+              lg: 8,
+            }}
+            mt={{
+              base: 10,
+              sm: 12,
+              md: 16,
+              lg: 20,
+              xl: 28,
+            }}
+          >
+            <Box
+              w="full"
+              textAlign={{
+                sm: "center",
+                lg: "left",
+              }}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <chakra.h1
+                fontSize={{
+                  base: "4xl",
+                  sm: "5xl",
+                  md: "6xl",
+                }}
+                letterSpacing="tight"
+                lineHeight="short"
+                fontWeight="extrabold"
+                color="gray.900"
+                _dark={{
+                  color: "white",
+                }}
+              >
+                <chakra.span
+                  display={{
+                    base: "block",
+                    xl: "inline",
+                  }}
+                >
+                 Freedom with{" "}
+                </chakra.span>
+                <chakra.span
+                  display={{
+                    base: "block",
+                    xl: "inline",
+                  }}
+                  color="brand.800"
+                  _dark={{
+                    color: "brand.400",
+                  }}
+                >
+                  Social Pilot
+                </chakra.span>
+              </chakra.h1>
+              <chakra.p
+                mt={{
+                  base: 3,
+                  sm: 5,
+                  md: 5,
+                }}
+                fontSize={{
+                  sm: "lg",
+                  md: "xl",
+                }}
+                maxW={{
+                  sm: "xl",
+                }}
+                mx={{
+                  sm: "auto",
+                  lg: 0,
+                }}
+                color="gray.500"
+              >
+              Our blockchain-powered social media platform puts you in control of your data, rewards you for your contributions, and connects you with a vibrant community of like-minded individuals. Experience the future of social media today.
+              </chakra.p>
+              <Box
+                mt={{
+                  base: 5,
+                  sm: 8,
+                }}
+                display={{
+                  sm: "flex",
+                }}
+                justifyContent={{
+                  sm: "center",
+                  lg: "start",
+                }}
+                fontWeight="extrabold"
+                fontFamily="sans-serif"
+              >
+                <Box rounded="full" shadow="md">
+                  <chakra.a
+                    w="full"
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    border="solid 1px transparent"
+                    fontSize={{
+                      base: "md",
+                      md: "2xl",
+                    }}
+                    rounded="md"
+                    color="white"
+                    bg="brand.900"
+                    _hover={{
+                      bg: "brand.800",
+                    }}
+                    px={{
+                      base: 8,
+                      md: 10,
+                    }}
+                    py={{
+                      base: 3,
+                      md: 4,
+                    }}
+                    cursor="pointer"
+                    onClick={handleLogin}
+                  >
+                    Connect Wallet
+                  </chakra.a>
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        position={{
+          lg: "absolute",
+        }}
+        top={{
+          lg: 0,
+        }}
+        bottom={{
+          lg: 0,
+        }}
+        right={{
+          lg: 0,
+        }}
+        w={{
+          lg: "50%",
+        }}
+        border="solid 1px transparent"
+      >
+        <Image
+          h={[56, 72, 96, "full"]}
+          w="full"
+          fit="cover"
+          src="https://images.pexels.com/photos/4549416/pexels-photo-4549416.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          loading="lazy"
+        />
+      </Box>
+    </Box>
+    )
+}
+
+export default LoginPage;
